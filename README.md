93cbdefba4175114b88b56f3f7a1e8ab0cbf6303
StepanPotiienko <kotnexjieb@protonmail.com>
2024-11-15 06:08:17 +0200

    data displays on mouse hover, removed frequency

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ## TODO
 
+- Implement functionality to change frequency.
+- Dark/Light theme.
+
 ## DONE
 
 - The height of the graph should change based on the slider's input (the scale should remain the same).
 - Move classes to different files.
 - Remove titlebar.
+- Data displays on mouse hover.
--- wave_modulator.py
+++ wave_modulator.py
@@ -1,13 +1,12 @@
 import numpy
 from scipy import signal
-
 import matplotlib.pyplot as pyplot
+import mplcursors
 
 
 class WaveModulator:
-    def __init__(self, voltage_amplitude, frequency):
+    def __init__(self, voltage_amplitude):
         self.voltage_amplitude = voltage_amplitude
-        self.frequency = frequency
         self._X = numpy.linspace(0, 4 * numpy.pi, 400)
 
     def sin(self):
@@ -40,21 +39,24 @@ class WaveModulator:
         ax.grid()
         ax.set_xlabel("Time (s)")
         ax.set_ylabel("Amplitude")
+
+        cursor = mplcursors.cursor(line, hover=True)
+        cursor.connect(
+            "add",
+            lambda sel: sel.annotation.set_text(
+                f"X: {sel.target[0]:.2f}\nY: {sel.target[1]:.2f}"
+            ),
+        )
+
         return fig, line
 
     def update_waveform(self, line, waveform):
         if waveform == "sine":
             Y = numpy.sin(self._X) * self.voltage_amplitude
         elif waveform == "square":
-            Y = (
-                signal.square(2 * numpy.pi * self.frequency * self._X)
-                * self.voltage_amplitude
-            )
+            Y = signal.square(2 * numpy.pi * self._X) * self.voltage_amplitude
         elif waveform == "sawtooth":
-            Y = (
-                signal.sawtooth(2 * numpy.pi * self.frequency * self._X)
-                * self.voltage_amplitude
-            )
+            Y = signal.sawtooth(2 * numpy.pi * self._X) * self.voltage_amplitude
         elif waveform == "dc":
             Y = numpy.ones_like(self._X) * self.voltage_amplitude
         else:
